344d0f681527788d8f8b0fca0203cd5c9a769805
Luke Tomlin <luke.tomlin@gmail.com>
2019-03-16 17:59:06 +0000

    started with vec stuff

--- src/main.rs
+++ src/main.rs
@@ -100,16 +100,17 @@ fn render_ppm(path: &Path, canvas: Canvas) {
 }
 
 struct Sphere {
-    position: (f64, f64),
+    position: Vector,
     radius: f64,
 }
 
 impl Sphere {
-    pub fn new(position: (f64, f64), radius: f64) -> Sphere {
+    pub fn new(position: Vector, radius: f64) -> Sphere {
         Sphere { position, radius }
     }
 }
 
+#[derive(Debug)]
 struct Vector {
     x: f64,
     y: f64,
@@ -125,24 +126,40 @@ impl Vector {
     pub fn new_with_length(x: f64, y: f64, z: f64, n: f64) -> Vector {
         Vector { x, y, z, n }
     }
+
+    pub fn cross(&self, other: &Vector) -> Vector {
+        let x = (self.y * other.z) - (self.z * other.y);
+        let y = (self.z * other.x) - (self.x * other.z);
+        let z = (self.x * other.y) - (self.y * other.x);
+
+        let n = ((x * x) + (y * y) + (z * z)).sqrt();
+
+        Vector::new_with_length(x, y, z, n)
+    }
 }
 
 struct Camera {
     plane_z: Vector,
     plane_x: Vector,
     pos: (f64, f64),
-    width: usize,
-    height: usize,
+    width: f64,
+    height: f64,
+    pixels_width: usize,
+    pixels_height: usize,
 }
 
 impl Camera {
-    pub fn new(width: usize, height: usize) -> Camera {
-        Canvas {
+    pub fn new(width: f64, height: f64, pixels_width: usize, pixels_height: usize) -> Camera {
+        let default_z = Vector::new(0.0, 0.0, 1.0);
+        let default_x = Vector::new(1.0, 0.0, 1.0);
+        Camera {
             plane_z: default_z,
             plane_x: default_x,
             pos: (0.0, 0.0),
             width,
             height,
+            pixels_width,
+            pixels_height,
         }
     }
 
@@ -162,9 +179,11 @@ impl Camera {
     }
 
     pub fn raytrace(&self, objects: Vec<Sphere>) -> Canvas {
-        let mut canvas = Canvas::new(self.width, self.height);
+        let mut canvas = Canvas::new(self.pixels_width, self.pixels_height);
 
-        for (x, y) in iproduct!(0..self.width, 0..self.height) {}
+        for (x, y) in iproduct!(0..self.pixels_width, 0..self.pixels_height) {
+            //let plane_y = 0
+        }
 
         canvas
     }
@@ -176,20 +195,54 @@ struct Scene {
 }
 
 impl Scene {
+    pub fn new(camera: Camera) -> Scene {
+        Scene {
+            objects: vec![],
+            camera,
+        }
+    }
+
+    pub fn add_object(&mut self, o: Sphere) -> &mut Self {
+        self.objects.push(o);
+        self
+    }
+
     pub fn raytrace(&self) -> Canvas {
         // iterate over each of the pixels
         // cast into the scene
         // see if intersect with any objects
         //self.camera.trace(&self.objects)
+        panic!()
     }
 }
 
 fn main() {
-    let mut canvas = Canvas::new(20, 20);
+    //let mut canvas = Canvas::new(20, 20);
 
-    for (x, y) in iproduct!(0..20, 0..20).step_by(2) {
-        canvas.ink(x, y);
-    }
+    //for (x, y) in iproduct!(0..20, 0..20).step_by(2) {
+    //    canvas.ink(x, y);
+    //}
+
+    /*
+    let sphere1 = Sphere::new(Vector::new(5.0, 5.0, 5.0), 1.0);
+    let sphere2 = Sphere::new(Vector::new(7.0, 7.0, 5.0), 0.2);
+    let sphere3 = Sphere::new(Vector::new(3.0, 3.0, 5.0), 0.5);
+
+    let camera = Camera::new(10.0, 10.0, 512, 512);
+
+    let mut scene = Scene::new(camera);
+    scene
+        .add_object(sphere1)
+        .add_object(sphere2)
+        .add_object(sphere3);
+
+    let canvas = scene.raytrace();
 
     render_ppm(Path::new("/tmp/out.pbm"), canvas);
+    */
+
+    let v1 = Vector::new(0.0, 0.0, 1.0);
+    let v2 = Vector::new(1.0, 0.0, 0.0);
+
+    println!("{:?} x {:?} = {:?}", v1, v2, v1.cross(&v2));
 }
